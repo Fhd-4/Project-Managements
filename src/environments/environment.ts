40891776135db2e3f491a66f74d27ea11547e524
Fhd-4 <fahadold91@gmail.com>
2026-07-29 11:07:20 +0300

    c

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://prosync-swagger.runasp.net/api',
+  apiUrl: 'https://prosync-swagger.runasp.net/api',
   endpoints: {
     auth: {
       login: '/Auth/login',
