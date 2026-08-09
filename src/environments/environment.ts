b0676ddd37aa93bba46690d8ccf7b6a8717a64d4
Fhd-4 <fahadold91@gmail.com>
2026-08-09 09:30:11 +0300

    c

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7130/api',
+  apiUrl: 'https://localhost:44319/api',
   endpoints: {
     auth: {
       login: '/Auth/login',
